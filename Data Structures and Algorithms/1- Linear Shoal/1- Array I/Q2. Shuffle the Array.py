469776cff093076841fd050d55f564c97ed397ff
Noran Mohamed <103522611+noran-mohamed@users.noreply.github.com>
2026-01-08 20:27:02 +0200

    Create Q2. Shuffle the Array.py

---
+++ Data Structures and Algorithms/1- Linear Shoal/1- Array I/Q2. Shuffle the Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = [nums[i//2] if i%2 == 0 else nums[i//2+n] for i in range(2*n)]
+        return res
+
+        
